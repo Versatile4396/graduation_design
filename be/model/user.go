8f6223a9a3c187696f34cea4044741f8abd7bc0b
huangmande <huangmande@bytedance.com>
2025-01-15 15:48:02 +0800

    feat: 删除老文件

--- be/model/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	UserName        string `json:"username" binding:"required"`  // 用户名
-	Email           string `json:"email" binding:"required"`     // 邮箱
-	Gender          int    `json:"gender" binding:"oneof=0 1 2"` // 性别 0:未知 1:男 2:女
-	Password        string `json:"password" binding:"required"`  // 密码
-	ConfirmPassword string `json:"confirm_password" binding:"required,eqfield=Password"`
-}
