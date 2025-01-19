1fad52d7a5d46484a1ea01344504d3edbc87f6b5
huangmande <huangmande@bytedance.com>
2025-01-19 21:16:29 +0800

    feat: 回归vue

--- fe/app-react/src/middlewares/AuthMiddleware/index.tsx
+++ fe/app-react/src/middlewares/AuthMiddleware/index.tsx
@@ -6,17 +6,7 @@ const AuthMiddleware = () => {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
-    setTimeout(() => {
-      // 随机返回 0 或 1
-      const authed = Math.round(Math.random());
-      console.log("auth authed", authed);
-      if (authed) {
-        setLoading(false);
-      } else {
-        // 未登录，跳转到登录页
-        navigate("/login");
-      }
-    }, 3000);
+    navigate("/login");
     console.log("AuthMiddleware 中间件触发");
   }, []);
   return !loading ? <Outlet /> : <Spin spinning />;
--- fe/app/src/page/test/index.vue
+++ fe/app/src/page/test/index.vue
@@ -1,43 +1,63 @@
 <template>
-  <FormProvider :form="form">
-    <SchemaField :schema="schema" />
+  <FormProvider :form="formInstance">
+    <Space>
+      <Field
+        name="price"
+        title="价格"
+        :initial-value="5.2"
+        :decorator="[FormItem]"
+        :component="[
+          InputNumber,
+          {
+            placeholder: '请输入',
+            style: {
+              width: 100,
+            },
+          },
+        ]"
+      />
+      <FormItem>×</FormItem>
+      <Field
+        name="count"
+        title="数量"
+        :initial-value="100"
+        :decorator="[FormItem]"
+        :component="[
+          InputNumber,
+          {
+            placeholder: '请输入',
+            style: {
+              width: 100,
+            },
+          },
+        ]"
+      />
+      <FormConsumer>
+        <template #default="{ form }">
+          <FormItem>
+            = {{ `${form.values.price * form.values.count} 元` }}</FormItem
+          >
+        </template>
+      </FormConsumer>
+    </Space>
   </FormProvider>
-  <Button @click="submitd">提交</Button>
 </template>
 
-<script setup>
-import { Input, FormItem } from "ant-design-vue";
+<script lang="ts">
 import { createForm } from "@formily/core";
-import { FormProvider, createSchemaField } from "@formily/vue";
-import { Button } from "ant-design-vue";
-const { SchemaField } = createSchemaField({
-  components: {
-    Input,
-    FormItem,
-  },
-});
+import { InputNumber, FormItem, Space } from "@formily/antdv-x3";
+import { FormProvider, FormConsumer, Field } from "@formily/vue";
 
-const form = createForm();
-const schema = {
-  type: "object",
-  properties: {
-    input: {
-      type: "string",
-      "x-decorator": "FormItem",
-      "x-component": "Input",
-      "x-component-props": {
-        placeholder: "请输入",
-      },
-      "x-validator": {
-        required: true,
-      },
-    },
+const formInstance = createForm();
+
+export default {
+  components: { FormProvider, FormConsumer, Field, FormItem, Space },
+  data() {
+    return {
+      formInstance,
+      InputNumber,
+      FormItem,
+    };
   },
-};
-const submitd = () => {
-  console.log("asd");
-  form.submit((v) => {
-    console.log(v);
-  });
 };
 </script>
